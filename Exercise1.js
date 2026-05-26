3fe78544e49a80dcdaedbf0cb75ee3538f8b29a0
Boitshoko-Mojela <bradboitshoko91@gmail.com>
2026-05-26 22:58:43 +0200

    Refactor Exercise 1 with updated variable values

---
+++ Exercise1.js
@@ -0,0 +1,12 @@
+// Exercise 1: Variable Declaration
+
+// 1-3. Declare variables with relevant types
+let name = "Boitshoko Mojela"; 
+let age = 24;                  
+let isStudent = true;          
+
+// 4. Print all three variables
+console.log("--- Exercise 1: Variable Declaration ---");
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
